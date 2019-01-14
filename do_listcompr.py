4b048e468087341cd4ec28dd6d528ba2f9ccbd1e
e <446916901@qq.com>
2019-01-14 21:06:12 +0800

    列表生成式V1.0

---
+++ do_listcompr.py
@@ -0,0 +1,10 @@
+#列表生成式
+
+listcompr_01 = list(range(1,11))
+print(listcompr_01)
+
+listcompr_02 = [x for x in range(1,10)]
+print(listcompr_02)
+
+listcompr_03 = [x * x for x in range(1,10)]
+print(listcompr_03)
