e5c9bf372a876feb212f5562a480679452464fe4
Noughton <446916901@qq.com>
2019-01-15 00:34:22 +0800

    列表生成式V1.1

--- do_listcompr.py
+++ do_listcompr.py
@@ -8,3 +8,13 @@ print(listcompr_02)
 
 listcompr_03 = [x * x for x in range(1,10)]
 print(listcompr_03)
+
+listcompr_04 = [x for x in range(1,10) if(x * x < 10)]
+print(listcompr_04)
+
+listcompr_05 = [x + y for x in '123' for y in '789']
+print(listcompr_05)
+
+distcompr_01 = {'name':'锤子','age':'23','ipone':'133'}
+listcompr_06 = [x + y for x,y in distcompr_01.items()]
+print(listcompr_06)
